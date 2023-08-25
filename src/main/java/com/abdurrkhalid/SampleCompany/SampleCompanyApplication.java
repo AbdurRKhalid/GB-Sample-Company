6cf20833f959106809add1513c394cf3c1fbcc1c
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2023-08-25 21:28:41 +0200

    Added the Docker-Compose and Application Properties Files

--- src/main/java/com/abdurrkhalid/SampleCompany/SampleCompanyApplication.java
+++ src/main/java/com/abdurrkhalid/SampleCompany/SampleCompanyApplication.java
@@ -2,12 +2,20 @@ package com.abdurrkhalid.SampleCompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SampleCompanyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleCompanyApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello!";
 	}
 
 }
