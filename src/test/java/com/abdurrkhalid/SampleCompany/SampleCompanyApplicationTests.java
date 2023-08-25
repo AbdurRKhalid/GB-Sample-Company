e7dc1bd8cbee35c4c0a8bbda31fd53ffc9298fbf
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2023-08-25 21:06:47 +0200

    Added the Base Project Files

---
+++ src/test/java/com/abdurrkhalid/SampleCompany/SampleCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdurrkhalid.SampleCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
