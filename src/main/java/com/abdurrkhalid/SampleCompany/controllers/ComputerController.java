ce4545a4b24b9c36efec1691b098424630a7c74d
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2023-08-26 14:44:47 +0200

    Added the Proper Response Entities as Response

--- src/main/java/com/abdurrkhalid/SampleCompany/controllers/ComputerController.java
+++ src/main/java/com/abdurrkhalid/SampleCompany/controllers/ComputerController.java
@@ -3,8 +3,11 @@ package com.abdurrkhalid.SampleCompany.controllers;
 import com.abdurrkhalid.SampleCompany.models.Computer;
 import com.abdurrkhalid.SampleCompany.services.ComputerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -14,13 +17,36 @@ public class ComputerController {
     ComputerService computerService;
 
     @PostMapping("/add")
-    public String addComputer(@RequestBody Computer computer) {
-        Computer computer1 = computerService.saveComputer(computer);
-        return "Added!";
+    public ResponseEntity addComputer(@RequestBody Computer computer) {
+        try {
+            HashMap response = new HashMap();
+            computerService.saveComputer(computer);
+            response.put("success", true);
+            response.put("message", "Computer Added Successfully to Database!");
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        } catch (Exception e) {
+            HashMap response = new HashMap();
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            response.put("message", "Error Occurred While Adding the Computer to Database!");
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/get-all")
-    public List getComputers() {
-        return computerService.getAllComputers();
+    public ResponseEntity getComputers() {
+        try {
+            HashMap response = new HashMap();
+            response.put("success", true);
+            response.put("data", computerService.getAllComputers());
+            response.put("message", "All Computers Fetched Successfully!");
+            return new ResponseEntity(response, HttpStatus.OK);
+        } catch (Exception e) {
+            HashMap response = new HashMap();
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            response.put("message", "Error Occurred While Fetching the Computers!");
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
     }
 }
