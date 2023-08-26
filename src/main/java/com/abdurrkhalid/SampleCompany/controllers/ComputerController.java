079fd9348e1c725a8e16fc600c3fedc3b714fff6
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2023-08-26 17:24:55 +0200

    Added the Controller to get Computers of Employee.

--- src/main/java/com/abdurrkhalid/SampleCompany/controllers/ComputerController.java
+++ src/main/java/com/abdurrkhalid/SampleCompany/controllers/ComputerController.java
@@ -53,4 +53,21 @@ public class ComputerController {
             return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("/get-all-by-employee/{abbreviation}")
+    public ResponseEntity getComputersByEmployee(@PathVariable String abbreviation) {
+        try {
+            HashMap response = new HashMap();
+            response.put("success", true);
+            response.put("data", computerService.findByAbbreviation(abbreviation));
+            response.put("message", "All Computers of Employee Fetched Successfully!");
+            return new ResponseEntity(response, HttpStatus.OK);
+        }catch (Exception e) {
+            HashMap response = new HashMap();
+            response.put("success", false);
+            response.put("error", e.getMessage());
+            response.put("message", "Error Occurred While Fetching the Computers!");
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
